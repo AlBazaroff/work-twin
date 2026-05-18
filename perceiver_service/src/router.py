251f06f71a48602f6a6544a63cdb2bda82b61f67
Alex Bazarov <bazaroffalex@gmail.com>
2026-05-18 20:58:30 +0300

    feat: added base health endpoints

--- perceiver_service/src/router.py
+++ perceiver_service/src/router.py
@@ -3,6 +3,17 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/")
-async def root():
-    return {"message": "Base endpoint for Perceiver Service"}
+@router.get("/health", tags=["health"])
+async def health():
+    """For checking service health."""
+    return {
+        "status": "ok",
+    }
+
+
+@router.get("/ready", tags=["health"])
+async def ready():
+    """For checking service health(alternative)."""
+    return {
+        "status": "ready",
+    }
